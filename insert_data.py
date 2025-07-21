95bd372dfc92eb1e5d378da7d0b707fefed43184
FoukharSara <foukharsara@gmail.com>
2025-07-21 12:41:33 +0100

    schema and table created

--- insert_data.py
+++ insert_data.py
@@ -1,5 +1,6 @@
 from streaming_data import  mock_data
 import json
+# download the psycopg2-binary package
 import psycopg2
 from dotenv import load_dotenv
 import os
@@ -16,21 +17,92 @@ def connect_to_db():
             host="localhost",
             port="5432"
         )
-        print(conn) 
-    except Exception as e:
+        return conn
+    except psycopg2.Error as e:
         print(f"An error occurred while connecting to the database: {e}")
-        return None
+        raise
 
-# def insert_data():
-#     data = mock_data()
-#     if data:
-#         print("Inserting data into the database...")
-#         try:
-            
+def create_schema(conn):
+    query = """
+        CREATE SCHEMA IF NOT EXISTS dev;
+    """
+    try:
+        print("Creating schema...")
+        cursor = conn.cursor()
+        cursor.execute(query)
+        conn.commit()
+        cursor.close()
+        print("Schema created successfully.")
+    except psycopg2.Error as e:
+        print(f"An error occurred while creating schema: {e}")
+        raise
+    
+def create_table(conn):
+    query= """
+    CREATE TABLE dev.raw_weather_data (
+    id SERIAL PRIMARY KEY,
+    
+    -- Location info
+    location_name VARCHAR(100),
+    region VARCHAR(100),
+    country VARCHAR(100),
+    lat FLOAT,
+    lon FLOAT,
+    tz_id VARCHAR(50),
+    local_time TIMESTAMP,
+    
+    -- Current weather info
+    last_updated TIMESTAMP,
+    temp_c REAL,
+    temp_f REAL,
+    is_day BOOLEAN,
+    condition_text VARCHAR(100),
+    condition_icon VARCHAR(255),
+    condition_code INTEGER,
+    wind_mph REAL,
+    wind_kph REAL,
+    wind_degree INTEGER,
+    wind_dir VARCHAR(10),
+    pressure_mb REAL,
+    pressure_in REAL,
+    precip_mm REAL,
+    precip_in REAL,
+    humidity INTEGER,
+    cloud INTEGER,
+    feelslike_c REAL,
+    feelslike_f REAL,
+    windchill_c REAL,
+    heatindex_f REAL,
+    dewpoint_c REAL,
+    dewpoint_f REAL,
+    vis_km REAL,
+    vis_miles REAL,
+    gust_mph REAL,
+    gust_kph REAL,
+    
+    recorded_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP 
+    );
+    """
+    try:
+        print("Creating Table...")
+        cursor = conn.cursor()
+        cursor.execute(query)
+        conn.commit()
+        print("Table created successfully.")
+        cursor.close()
+    
+    except psycopg2.Error as e:
+        print(f"An error occurred while creating table: {e}")
+        raise
+
+
         
-#         except Exception as e:
-#             print(f"An error occurred while inserting data: {e}")
-#     else:
-#         print("No data to insert.")
-        
-connect_to_db()
+conn = connect_to_db()
+if conn:
+    print("Connected to the database successfully.")
+    create_schema(conn)
+    create_table(conn)
+    
+    conn.close()
+    print("Connection closed.")
+
