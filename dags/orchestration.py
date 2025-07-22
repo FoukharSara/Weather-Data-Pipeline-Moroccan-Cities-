610098fcee6b604de44ccd81479ad1e0308d7ef4
FoukharSara <foukharsara@gmail.com>
2025-07-22 12:07:52 +0100

    merge the dags into one

--- dags/orchestration.py
+++ dags/orchestration.py
@@ -1,6 +1,9 @@
 from airflow import DAG
 from datetime import datetime, timedelta
 from airflow.operators.python import PythonOperator
+from docker.types import Mount
+from airflow.providers.docker.operators.docker import DockerOperator
+
 
 
 
@@ -11,7 +14,7 @@ default_args = {
 }
 
 dag =  DAG(
-    "orchestration_weather",
+    "orchestration_weather_api_dbt",
     default_args=default_args,
     catchup=False,
     schedule= timedelta(minutes=1),
@@ -27,3 +30,28 @@ with dag:
         python_callable=data_ingestion,
     )
     #task2
+    
+    task2 = DockerOperator(
+        task_id="dbt_run",
+        image="fishtownanalytics/dbt:1.0.0",
+        command="run",
+        docker_url="unix://var/run/docker.sock",
+        network_mode="weatherdatapipeline_morccan_confluent",
+        working_dir="/usr/app/dbt_project",
+        mounts=[
+            Mount(
+                source="C:/Users/x1 carbon/Desktop/Weather Data Pipeline_Morccan/dbt_project",
+                target="/usr/app/dbt_project",
+                type="bind"
+            ),
+            Mount(
+                source="C:/Users/x1 carbon/Desktop/Weather Data Pipeline_Morccan/profiles.yml",
+                target="/root/.dbt/profiles.yml",
+                type="bind"
+            )
+                
+        ],
+        auto_remove='success',
+    )
+    
+    task1 >> task2
